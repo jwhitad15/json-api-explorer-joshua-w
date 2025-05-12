87f5506733f6e6f5120239d6de5586b472e01a52
Joshua <jwhite83193@gmail.com>
2025-05-11 21:21:39 -0500

    First crack at API fetching

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-// so much empty
+const fetchData = fetch("https://jsonplaceholder.typicode.com/posts");
+
+fetchData.then(function(response){
+    const jsonData = response.json();
+    jsonData.then(function(json){
+        console.log("Data:", json.title);
+    });
+});
+
+const div = document.getElementById("postList");
+    div.innerHTML = `
+        <ul>
+            <li>Title: ${json.title}</li>
+            <li>Body: ${json.body} </li>
+        </ul> `;
