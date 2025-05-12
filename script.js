b876c5aadf6b2c75f2880b57de6f5e262c6faef5
Joshua <jwhite83193@gmail.com>
2025-05-11 21:48:56 -0500

    from solutions

--- script.js
+++ script.js
@@ -7,9 +7,42 @@ fetchData.then(function(response){
     });
 });
 
-const div = document.getElementById("postList");
-    div.innerHTML = `
-        <ul>
-            <li>Title: ${json.title}</li>
-            <li>Body: ${json.body} </li>
-        </ul> `;
+const postList= document.getElementById("postList");
+const postForm = document.getElementById("postForm");
+
+const renderPosts = (posts) => {
+    postList.innerHTML = "";
+    posts.forEach((post) => {
+        const postElement = document.createElement("div");
+        postElement.innerHTML = `
+            <h3>${post.title}</h3>
+            <p>${post.body}</p>
+            <hr />`;
+        postList.appendChild(postElement);
+    });
+
+  
+fetch("https://jsonplaceholder.typicode.com/posts?_limit=5")
+        .then((response) => response.json())
+        .then((data) => renderPosts(data))
+        .catch((error) => console.error("Error fetching posts:", error));
+
+    postForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+        const title = document.getElementById("title").value;
+        const body = document.getElementById("body").value;
+
+fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    headers: {"Content-type": "application/json"},
+    body: JSON.stringify({ title, body })
+    })
+    .then((response) => response.json())
+    .then((newPost) => {
+    alert("Post submitted!");
+    renderPosts([newPost]); 
+    })
+    .catch((error) => console.error("Error submitting post:",
+   error));
+    });
+};
